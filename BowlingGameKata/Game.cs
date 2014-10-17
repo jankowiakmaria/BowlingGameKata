6c67a02b10f6ebc9dc8cc4b5d51ca9915ba4a687
jankowiakmaria <jankowiakmaria@gmail.com>
2014-10-17 13:09:03 +0200

    init

---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        public int Roll(int pins)
+        {
+            return 0;
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
